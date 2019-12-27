8bbbdafe7c6cb4c5eb8c7596cfffdc25ad5e420d
Nethelbert Blasse <gilblasse@gmail.com>
2019-12-27 03:42:52 -0500

    added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
 
-passenger = Passenger.create(name: Faker::Name.unique.name)
-driver = Driver.create(name: Faker::Name.unique.name)
+10.times do 
+    passenger = Passenger.create(name: Faker::Name.unique.name)
+    driver = Driver.create(name: Faker::Name.unique.name)
 
-trip = passenger.create_trip(
-            origin_title: Faker::Company.unique.name,
-            origin_address: Faker::Address.unique.full_address,
-            destination_title: Faker::Company.unique.name,
-            destination_address: Faker::Address.unique.full_address
-        )
-driver.trips << trip
+    trip = passenger.create_trip(
+                origin_title: Faker::Company.unique.name,
+                origin_address: Faker::Address.unique.full_address,
+                destination_title: Faker::Company.unique.name,
+                destination_address: Faker::Address.unique.full_address
+            )
+    driver.trips << trip
 
-passenger.rate_driver({comment: Faker::Lorem.unique.sentence,stars: rand(1..5) },trip)
-driver.rate_passenger({comment: Faker::Lorem.unique.sentence,stars: rand(1..5) },trip)
+    passenger.rate_driver({comment: Faker::Lorem.unique.sentence,stars: rand(1..5) },trip)
+    driver.rate_passenger({comment: Faker::Lorem.unique.sentence,stars: rand(1..5) },trip)
+end
+puts "Seeding Data Completed..."
